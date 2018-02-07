ccc8d557392bcfdc7d02d2a300fbca28d74d71be
Kowshik Sundararajan <kowshik@versafleet.co>
2018-02-07 11:16:11 +0800

    Read raw html from file

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+const _ = require('lodash');
 const fs = require('fs');
 const path = require('path');
 const htmlparser = require('htmlparser');
 
-const raw = '<img alt="hello" /> <img /><p></p>';
-
-let tags = [];
+const FILE_PATH = './index.html';
+const tags = [];
 
 const handler = new htmlparser.DefaultHandler((error, dom) => {
   if (error) console.error(error);
@@ -19,10 +19,34 @@ const handler = new htmlparser.DefaultHandler((error, dom) => {
 }, { ignoreWhitespace: true });
 
 const parser = new htmlparser.Parser(handler);
-parser.parseComplete(raw);
 
-let count = 0;
-tags.forEach((item) => {
-  if (item.name === 'img' && !item.attribs.alt) count += 1;
+fs.readFile(FILE_PATH, (error, data) => {
+  if (error) console.error(error);
+  else {
+    parser.parseComplete(data);
+    let ruleOneCount = 0;
+    let ruleTwoCount = 0;
+    let ruleThreeCount = 0;
+    let ruleFourCount = 0;
+    let ruleFiveCount = 0;
+    tags.forEach((item) => {
+      if (item.name === 'img' && !_.has(item.attribs, 'alt')) ruleOneCount += 1;
+      else if (item.name === 'a' && !_.has(item.attribs, 'rel')) { console.log(item); ruleTwoCount += 1; }
+      else if (item.name === 'head') {
+        if (item.children) {
+          let hasTitle = false;
+          item.children.forEach((child) => {
+            if (child.name === 'title') hasTitle = true;
+          });
+          if (!hasTitle) ruleThreeCount += 1;
+        }
+      }
+      console.log(item);
+    });
+    console.log(`There are ${ruleOneCount} <img> tags without alt attribute`);
+    console.log(`There are ${ruleTwoCount} <a> tags without rel attribute`);
+    console.log(`There are ${ruleThreeCount} <a> tags without rel attribute`);
+    console.log(`There are ${ruleFourCount} <a> tags without rel attribute`);
+    console.log(`There are ${ruleFiveCount} <a> tags without rel attribute`);
+  }
 });
-console.log(`There are ${count} <img> tags without alt attribute`);
